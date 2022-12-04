7760a6254bce6a2d564ed3a5304c447adf863fd7
Alex McKendry <a.mckendry2@gmail.com>
2022-12-04 16:19:19 -0500

    changed formatting of day 03 answer

--- 03/solution.py
+++ 03/solution.py
@@ -1,13 +1,16 @@
 import sys
 
-def getDupe(f, s): return f[next(i for i in range(len(f)) if f[i] in s)]
-def getBadge(f, s, t): return f[next(i for i in range(len(f)) if f[i] in s and f[i] in t)]
-def charScore(c): return (s - 38) if (s := ord(c)) < 97 else s - 96
-
 lines = sys.stdin.readlines()
-groups = [lines[i:i+3] for i in range(0, len(lines), 3)]
+
+def getDupe(f, s): 
+  return f[next(i for i in range(len(f)) if f[i] in s)]
+def getBadge(f, s, t): 
+  return f[next(i for i in range(len(f)) if f[i] in s and f[i] in t)]
+def charScore(c): 
+  return (s - 38) if (s := ord(c)) < 97 else s - 96
+
 p1 = sum([charScore(getDupe(l[:(half := len(l)//2)],l[half:])) for l in lines])
-p2 = sum([charScore(getBadge(*g)) for g in groups])
+p2 = sum([charScore(getBadge(*g)) for g in (lines[i:i+3] for i in range(0, len(lines), 3))])
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
