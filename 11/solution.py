16ed3c6b04e52eb209e9d1e1b6add54406b08d15
Alex McKendry <alex@tallpoppy.com>
2022-12-12 12:09:53 -0500

    switched to data-oriented solution

--- 11/solution.py
+++ 11/solution.py
@@ -1,55 +1,64 @@
 import sys
 import re
 from functools import reduce
-from dataclasses import dataclass
-from typing import Callable
 
-jungle = []
+monkeys = []
+items, rawItems = [], []
+possessions = [] 
 
-class Item:
-  def __init__(self, val, mods):
-    self.mods = {m: val % m for m in mods}
-    self.basicVal = val
-  def updateMods(self, func):
-    self.mods = {k: (func(v) % k) for k,v in self.mods.items()}
-    self.basicVal = func(self.basicVal) // 3
+# funcs ==========
+def updateItem(idx, mon, p1):
+  math = (lambda l, r: l + r) if mon['op'] == '+' else (lambda l, r: l * r)
+  func = lambda n: math(n, n if mon['right'] == 'old' else int(mon['right']))
+  if p1:
+    rawItems[idx] = func(rawItems[idx]) // 3
+  else:
+    for k, v in items[idx].items():
+      items[idx][k] = func(v) % k
 
-@dataclass
-class Monkey:
-  items: list
-  op: Callable
-  right: str
-  testMod: int
-  choices: list
-  inspections: int = 0
+def testItem(idx, mon, p1): 
+  return (rawItems[idx] % mon['mod'] if p1 else items[idx][mon['mod']]) == 0
 
-  def doTurn(self, isPart1):
-    for i in self.items:
-      i.updateMods(lambda v: self.op(v, v if self.right == 'old' else int(self.right)))
-      testVal = i.basicVal % self.testMod if isPart1 else i.mods[self.testMod]
-      jungle[self.choices[testVal == 0]].items.append(i)
-      self.inspections += 1
-    self.items = []
-  
+def doTurn(p1):
+  for mIdx, monItems in enumerate(possessions):
+    monkey = monkeys[mIdx]
+    for item in  monItems:
+      updateItem(item, monkey, p1)
+      possessions[monkey['choices'][testItem(item, monkey, p1)]].append(item)
+      monkey['inspections'] += 1
+    possessions[mIdx] = []
+
+#grok data ==========
 lines = sys.stdin.readlines()
 mods = [int(i) for i in re.findall('divisible by (\d+)', ''.join(lines))]
-monkeys = [lines[i-7:i-1] for i in range(7, len(lines) + 2, 7)]
+monkeyData = [lines[i-7:i-1] for i in range(7, len(lines) + 2, 7)]
 
-def resetJungle():
-  jungle.clear()
-  for m in monkeys:
-    items = [Item(int(val), mods) for val in re.findall('\d+', m[1])]
-    sym = re.search("new = old (\S) (\S+)", m[2]) 
+def init():
+  monkeys.clear()
+  possessions.clear()
+  items.clear()
+  rawItems.clear()
+  for m in monkeyData:
+    sym = re.search("new = old (\S) (\S+)", m[2])
     nums = [int(i) for i in re.findall('\d+', m[3] + m[4] + m[5])]
-    op = (lambda l, r: l + r) if sym[1] == '+' else (lambda l, r: l * r)
-    jungle.append(Monkey(items, op, sym[2], nums[0], [nums[2], nums[1]]))
+    monkeys.append({
+      'op':sym[1],
+      'mod': nums[0],
+      'right': sym[2],
+      'choices': [nums[2], nums[1]],
+      'inspections': 0
+    })
+    possessions.append([])
+    for item in [int(i) for i in re.findall('\d+', m[1])]:
+      items.append({ mod: item % mod for mod in mods })
+      rawItems.append(item)
+      possessions[len(monkeys) - 1].append(len(items) - 1)
 
-#main
-resetJungle()
-for _ in range(20): 
-  for m in jungle: m.doTurn(True)
-print(f'Part 1: {reduce(lambda a, b: a * b, sorted(m.inspections for m in jungle)[-2:])}')
-resetJungle()
-for _ in range(10000):
-  for m in jungle: m.doTurn(False)
-print(f'Part 2: {reduce(lambda a, b: a * b, sorted(m.inspections for m in jungle)[-2:])}')
+
+#main ==========
+init()
+for _ in range(20): doTurn(True)
+print(reduce(lambda a,b: a*b, sorted(m['inspections'] for m in monkeys)[-2:]))
+init()
+for _ in range(10000): doTurn(False)
+print(reduce(lambda a,b: a*b, sorted(m['inspections'] for m in monkeys)[-2:]))
