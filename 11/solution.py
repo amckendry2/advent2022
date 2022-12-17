0b6825f0cf2aa913177fe3b421d3f9adb184c875
Alex McKendry <alex@tallpoppy.com>
2022-12-17 13:08:39 -0500

    improved day 11 solution

--- 11/solution.py
+++ 11/solution.py
@@ -1,32 +1,21 @@
 import sys
 import re
 from functools import reduce
+from types import SimpleNamespace
 
 monkeys = []
-items, rawItems = [], []
-possessions = [] 
+magicNum = 1
 
 # funcs ==========
-def updateItem(idx, mon, p1):
-  math = (lambda l, r: l + r) if mon['op'] == '+' else (lambda l, r: l * r)
-  func = lambda n: math(n, n if mon['right'] == 'old' else int(mon['right']))
-  if p1:
-    rawItems[idx] = func(rawItems[idx]) // 3
-  else:
-    for k, v in items[idx].items():
-      items[idx][k] = func(v) % k
-
-def testItem(idx, mon, p1): 
-  return (rawItems[idx] % mon['mod'] if p1 else items[idx][mon['mod']]) == 0
-
 def doTurn(p1):
-  for mIdx, monItems in enumerate(possessions):
-    monkey = monkeys[mIdx]
-    for item in  monItems:
-      updateItem(item, monkey, p1)
-      possessions[monkey['choices'][testItem(item, monkey, p1)]].append(item)
-      monkey['inspections'] += 1
-    possessions[mIdx] = []
+  for mon in monkeys:
+    for item in  mon['items']:
+      math = (lambda l, r: l + r) if mon['op'] == '+' else (lambda l, r: l * r)
+      func = lambda n: math(n, n if mon['right'] == 'old' else int(mon['right']))
+      newVal = (func(item) // 3) if p1 else (func(item) % magicNum)
+      monkeys[mon['choices'][newVal % mon['mod'] == 0]]['items'].append(newVal)
+      mon['inspections'] += 1
+    mon['items'] = []
 
 #grok data ==========
 lines = sys.stdin.readlines()
@@ -34,31 +23,25 @@ mods = [int(i) for i in re.findall('divisible by (\d+)', ''.join(lines))]
 monkeyData = [lines[i-7:i-1] for i in range(7, len(lines) + 2, 7)]
 
 def init():
+  global magicNum
   monkeys.clear()
-  possessions.clear()
-  items.clear()
-  rawItems.clear()
   for m in monkeyData:
     sym = re.search("new = old (\S) (\S+)", m[2])
     nums = [int(i) for i in re.findall('\d+', m[3] + m[4] + m[5])]
+    magicNum *= nums[0]
     monkeys.append({
       'op':sym[1],
       'mod': nums[0],
       'right': sym[2],
       'choices': [nums[2], nums[1]],
-      'inspections': 0
-    })
-    possessions.append([])
-    for item in [int(i) for i in re.findall('\d+', m[1])]:
-      items.append({ mod: item % mod for mod in mods })
-      rawItems.append(item)
-      possessions[len(monkeys) - 1].append(len(items) - 1)
-
+      'items': [int(i) for i in re.findall('\d+', m[1])],
+      'inspections': 0 })
 
 #main ==========
 init()
 for _ in range(20): doTurn(True)
-print(reduce(lambda a,b: a*b, sorted(m['inspections'] for m in monkeys)[-2:]))
+print('Part1:',reduce(lambda a,b: a*b, sorted(m['inspections'] for m in monkeys)[-2:]))
+
 init()
 for _ in range(10000): doTurn(False)
-print(reduce(lambda a,b: a*b, sorted(m['inspections'] for m in monkeys)[-2:]))
+print('Part2:',reduce(lambda a,b: a*b, sorted(m['inspections'] for m in monkeys)[-2:]))
